b53f901b642f46b9f46868362b7e063c120011ae
abhiram-karanth-core <abhiramkaranth22@gmail.com>
2026-03-15 10:01:09 +0530

    init ONNX embedding model at runtime

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 
+	"turbo-query/internal/embed"
 	"turbo-query/internal/server"
 )
 
 func main() {
+	if err := embed.Init(); err != nil {
+		log.Fatalf("failed to init embedding model: %v", err)
+	}
+
 	srv := server.NewServer()
 
 	log.Println("starting shard server on", srv.Addr)
